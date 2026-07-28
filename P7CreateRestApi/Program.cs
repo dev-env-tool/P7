3852790940a1cded43d8f51743de6898783100b5
260961435+dev-env-tool@users.noreply.github.com <260961435+dev-env-tool@users.noreply.github.com>
2026-07-28 14:46:47 +0200

    Domain : classes initialization before migration

--- P7CreateRestApi/Program.cs
+++ P7CreateRestApi/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<LocalDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("P7Referential")));
 
 var app = builder.Build();
 
